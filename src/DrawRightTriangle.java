758631a6d1769e25de48783953d14c3e40f04d69
siddharthsoni <siddharthsoni993@gmail.com>
2016-07-07 21:08:04 +0530

    Changed the request for input message

---
+++ src/DrawRightTriangle.java
@@ -0,0 +1,30 @@
+import com.sun.xml.internal.ws.util.StringUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by siddharth on 7/7/16.
+ */
+public class DrawRightTriangle {
+    public static void main(String args[])throws IOException{
+        System.out.println("Enter the number of lines");
+        String input = new BufferedReader(new InputStreamReader(System.in)).readLine();
+        int n = Integer.parseInt(input);
+        new DrawRightTriangle().drawRightTriangle(n);
+    }
+    private void drawRightTriangle(int n){
+        for(int i=0;i<n;i++){
+            printAsterisk(i+1);
+            System.out.println();
+        }
+    }
+
+    private void printAsterisk(int n){
+        if (n>0){
+        System.out.print('*');
+        printAsterisk(n-1);
+        }
+    }
+}
