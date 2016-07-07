c6a81ae9190c22882dfeb6e07f5416bb757059f2
siddharthsoni <siddharthsoni993@gmail.com>
2016-07-07 19:02:53 +0530

    DrawVerticalLIne

---
+++ src/DrawVerticalLine.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by siddharth on 7/7/16.
+ */
+public class DrawVerticalLine {
+    public static void main(String args[])throws IOException{
+        String input = new BufferedReader(new InputStreamReader(System.in)).readLine();
+        int n = Integer.parseInt(input);
+        drawVerticalLine(n);
+    }
+    private void drawVerticalLine(int ){
+
+    }
+}
