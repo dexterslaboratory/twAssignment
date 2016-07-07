61c5ec60372965da1687ff439093a16992978b2c
siddharthsoni <siddharthsoni993@gmail.com>
2016-07-07 18:19:33 +0530

    EasiestExercise

---
+++ src/EasiestExerciseEver.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+/**
+ * Created by siddharth on 7/7/16.
+ */
+
+public class EasiestExerciseEver {
+    public static void main(String args[])throws IOException{
+        System.out.print('*');
+    }
+}
